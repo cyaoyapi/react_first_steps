0ce54fde84ddb08cb15b981a294d700e77446cd1
Cyrille YAO <cyaoyapi@gmail.com>
2019-02-23 13:28:43 +0000

    Split component Table into 2 simples(functional) componets : TableHeader and TableBody

--- src/Table.js
+++ src/Table.js
@@ -1,34 +1,46 @@
 import React, { Component } from 'react';
 
+const TableHeader = function(){
+	return (
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Job</th>
+			</tr>
+		</thead>	
+	);
+};
+
+const TableBody = () => {
+	return (
+		<tbody>
+			<tr>
+				<td>Cyrille</td>
+				<td>Computer Scientist</td>
+			</tr>
+			<tr>
+				<td>Agnes</td>
+				<td>Cosmetologist</td>
+			</tr>
+			<tr>
+				<td>Dabira</td>
+				<td>Business woman</td>
+			</tr>
+			<tr>
+				<td>Akiva</td>
+				<td>Man of God</td>
+			</tr>				
+		</tbody>		
+	);
+};
+
 class Table extends Component{
 
 	render(){
 		return (
 			<table>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Job</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>Cyrille</td>
-						<td>Computer Scientist</td>
-					</tr>
-					<tr>
-						<td>Agnes</td>
-						<td>Cosmetologist</td>
-					</tr>
-					<tr>
-						<td>Dabira</td>
-						<td>Business woman</td>
-					</tr>
-					<tr>
-						<td>Akiva</td>
-						<td>Man of God</td>
-					</tr>				
-				</tbody>			
+				<TableHeader />
+				<TableBody />
 			</table>
 		);
 	}
