75127eb15b0f5c9a1a50a0273c15ebfa374e7c51
Cyrille YAO <cyaoyapi@gmail.com>
2019-02-23 16:58:55 +0000

    Updates

---
+++ precache-manifest.bbae43418d273f7a58db43d40e781b31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e433d801ae3dca7200fc",
+    "url": "/react_first_steps/static/css/main.064413c3.chunk.css"
+  },
+  {
+    "revision": "e433d801ae3dca7200fc",
+    "url": "/react_first_steps/static/js/main.e433d801.chunk.js"
+  },
+  {
+    "revision": "8bdbdb792ebdf4732c4e",
+    "url": "/react_first_steps/static/js/runtime~main.8bdbdb79.js"
+  },
+  {
+    "revision": "14bf863fa5da7fbc35b2",
+    "url": "/react_first_steps/static/js/2.14bf863f.chunk.js"
+  },
+  {
+    "revision": "dba372cf39994c8b00d3ce8ae26686de",
+    "url": "/react_first_steps/index.html"
+  }
+];
