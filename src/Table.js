a4a004b2923f8cf296f1b00410790d716da7d638
Cyrille YAO <cyaoyapi@gmail.com>
2019-02-23 13:18:26 +0000

    Add class component : Table

---
+++ src/Table.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Table extends Component{
+
+	render(){
+		return (
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Job</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Cyrille</td>
+						<td>Computer Scientist</td>
+					</tr>
+					<tr>
+						<td>Cyrille</td>
+						<td>Cosmetologist</td>
+					</tr>
+					<tr>
+						<td>Dabira</td>
+						<td>Business woman</td>
+					</tr>
+					<tr>
+						<td>Akiva</td>
+						<td>Man of God</td>
+					</tr>				
+				</tbody>			
+			</table>
+		);
+	}
+}
+
+export default Table;
