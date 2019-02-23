44766472089d9f0049fa3a77243a08de4f56b4c6
Cyrille YAO <cyaoyapi@gmail.com>
2019-02-23 13:19:34 +0000

    Rendering Table component in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
+import Table from './Table';
+
 class App extends Component{
 
 	render(){
 		return (
 			<div className="container">
 				<h1>Hello World! I'am learning React.js</h1>
+				<h2>My Family</h2>
+				<Table />
 			</div>
 		);
 	}
