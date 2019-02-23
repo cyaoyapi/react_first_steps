341af6d675bdc0b9f2cfc5334cc223ac7bc008d8
Cyrille YAO <cyaoyapi@gmail.com>
2019-02-23 13:03:03 +0000

    First component : Hello World

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+
+	render(){
+		return (
+			<div className="container">
+				<h1>Hello World! I'am learning React.js</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
